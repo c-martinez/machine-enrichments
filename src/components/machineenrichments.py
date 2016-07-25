8aaaadec439b21af1f2279837820e206f0df2e2f
Carlos Martinez <c.martinez@esciencecenter.nl>
2016-07-25 11:52:37 +0200

    Add provenance

--- src/components/machineenrichments.py
+++ src/components/machineenrichments.py
@@ -7,6 +7,7 @@ class MachineEnrichments():
         self._counter = 0       # TODO: generate unique tickets
         self._dbStatus = {}     # TODO: use real DB (sqlite at least?)
         self._dbResults = {}
+        self._dbProv = {}
         self._dbOrigIds = {}
 
         self._nerdClient = NERD ('nerd.eurecom.fr', settings.nerd_api_key)
@@ -14,6 +15,9 @@ class MachineEnrichments():
     def getNextTicketId(self):
         self._counter += 1
         return 'ticket-' + str(self._counter)
+
+    def getNextActivityId(self):
+        return 'activity-' + str(self._counter)
 
     def processNerd(self, itemId, content):
         content = _packJSONDict(content)
@@ -31,7 +35,6 @@ class MachineEnrichments():
         }
         return status
 
-
     def _NERDThread(self, text, ticketID):
         self._dbStatus[ticketID] = 'pending';
 
@@ -47,6 +50,9 @@ class MachineEnrichments():
             # After NERD has completed...
             self._dbResults[ticketID] = extractedData
             self._dbStatus[ticketID] = 'ready'
+            self._dbProv[ticketID] = {
+                'activityId': self.getNextActivityId()
+            }
         except Exception:
             self._dbResults[ticketID] = []
             self._dbStatus[ticketID] = 'failed'
@@ -59,19 +65,29 @@ class MachineEnrichments():
         }
 
     def collectAnnotation(self, ticketID):
-        provenace = {
-            'Tool': 'NERD'
-        }
         itemId = self._dbOrigIds[ticketID] if ticketID in self._dbOrigIds else None
         content = {}
         if ticketID in self._dbResults:
             entities = self._dbResults[ticketID]
             content['Entities'] = entities
+        prov = {}
+        if ticketID in self._dbProv:
+            itemProv = self._dbProv[ticketID]
+            itemProv['activityId']
+            prov = {
+                'prov:wasAttibutedTo': 'dive:nerdTool',
+                'prov:wasGeneratedBy': 'dive:myActivity' + itemProv['activityId'],
+                'dive:settings': {
+                    'URL': 'nerd.eurecom.fr',
+                    'extractor': 'combined',
+                    'timeout': 30
+                }
+            }
 
         return {
           "ticket": ticketID,
           "provenance": {
-            "data": _unpackJSONDict(provenace)
+            "data": _unpackJSONDict(flattenNestedDict(prov))
           },
           "data": [
             {
@@ -80,6 +96,51 @@ class MachineEnrichments():
             }
           ]
         }
+
+def flattenNestedDict(d, base=''):
+    '''The following dictionary:
+        {
+            'a1': {
+                'b1': 'x'
+            },
+            'a2': {
+                'b1': 'x',
+                'b2': 'y',
+                'b3': 'z',
+            },
+            'a3': {
+                'b1': 'x'
+                'b2': 'x',
+                'b3': {
+                    'c1': 1,
+                    'c2': 1
+                },
+            }
+        }
+
+    Will yield the following flat dict:
+        {
+            'a1.b1': 'x'
+            'a2.b1': 'x'
+            'a2.b2': 'y'
+            'a2.b3': 'z'
+            'a3.b1': 'x'
+            'a3.b2': 'x'
+            'a3.b3.c1': 1
+            'a3.b3.c2': 1
+        }
+    '''
+    if not isinstance(d, dict):
+        raise Exception('Cannot flatten non-dict')
+
+    flatDict = {}
+    for key, value in d.iteritems():
+        if isinstance(value, dict):
+            tmp = flattenNestedDict(value, base=base + key + '.')
+            flatDict.update(tmp)
+        else:
+            flatDict[base + key] = value
+    return flatDict
 
 def _packJSONDict(jsonDict):
     ''' Convert JSON list of key-value pairs to python dictionary '''
